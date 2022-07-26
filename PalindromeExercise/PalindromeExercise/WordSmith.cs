e79a1862820158748d1fc3d52987173d71139556
Travis Gersch <t_gersch@hotmail.com>
2022-07-26 10:01:20 -0500

    palindrome exercise finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null || word == "")
+            {
+                return false;
+            }
+
+            //reverse
+            //var reverse = "";
+
+            //for (int i = word.Length-1; i >= 0 ; i--)
+            //{
+            //    reverse += word[i];
+            //}
+            ////compare
+            //if (reverse.ToLower() == word.ToLower())
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            ////return
+
+            //var reverse = word.Reverse();
+
+            //if (reverse == word)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            if (String.IsNullOrEmpty(word)) //eliminates null or empty immediately
+            {
+                return false;
+            }
+
+            var specialCharacters = " ?!,.@#%&";
+            for (int i = 0; i < specialCharacters.Length; i++)  //converts special characters to nothing to take its place
+            {
+                word = word.Replace(specialCharacters[i].ToString(), ""); 
+            }
+            if (word == "")  //if converted special character string is nothing after converstion, false
+            {
+                return false;
+            }
+
+            else
+            {
+                char[] wordArray = word.ToCharArray(); //convert string to character array, then reverse array
+                Array.Reverse(wordArray);
+                string arrayReversedToString = new string(wordArray); //then reversed array to string
+                if (word.ToLower() == arrayReversedToString.ToLower()) //then verify new reversed string equals original string
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,26 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("red rum, sir, is murder", true)]
+        [InlineData("final", false)]
+        [InlineData("fantasy", false)]
+        [InlineData("sagas", true)]
+        [InlineData("Never odd or even.", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var test = new WordSmith();
+
+            //Act
+            var actual = test.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
